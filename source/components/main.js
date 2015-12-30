aa61e939d961149a325d4e1df036ed06904a7782
Debashish Sahoo <debashish4@gmail.com>
2015-12-30 22:21:44 +0530

    header navbar in progress

--- source/components/main.js
+++ source/components/main.js
@@ -1,10 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Dashboard = require('./Dashboard/Dashboard');
+var $ = jQuery = require('jquery');
+var BootstrapJS = require('./External/bootstrap.min');
+var Dashboard = require('./Dashboard/Dashboard'); 
 
  
 
 
 
-ReactDOM.render(<Dashboard/>, document.getElementById("app"));  
+ReactDOM.render(<Dashboard/>, document.getElementById("app"));   
  
