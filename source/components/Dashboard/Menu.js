2cd7d780efba972f6b0b2305430d27501c98872c
Debashish Sahoo <debashish4@gmail.com>
2015-12-30 15:40:11 +0530

    Dashboard header component created

---
+++ source/components/Dashboard/Menu.js
@@ -0,0 +1,27 @@
+var React = require('react');
+
+var Menu = React.createClass({
+	render: function() {  
+		return (
+      <div className="Menu"> 
+        <div>
+          <ul>
+            <li>1</li>  
+            <li>1</li>
+            <li>1</li>
+            <li>1</li>
+            <li>1</li>
+            <li>1</li>
+            <li>1</li>
+          </ul>
+
+        </div>
+
+      </div>
+
+      );  
+	}
+	
+});
+	
+module.exports = Menu;
--- source/components/main.js
+++ source/components/main.js
@@ -2,7 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Dashboard = require('./Dashboard/Dashboard');
 
-
+ 
 
 
 
